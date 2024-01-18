f42136398f0e3544ce4b930eb7b121ee265cd95e
isharab <155580193+sherijigarx@users.noreply.github.com>
2024-01-18 22:52:25 +0500

    Update tts.py

--- classes/tts.py
+++ classes/tts.py
@@ -171,7 +171,7 @@ class TextToSpeechService(AIModelService):
     def process_response(self, axon, response, prompt):
         try:
             speech_output = response.speech_output
-            if response.dendrite.status_code != 200 or self.response.dendrite.status_code != 403:
+            if response.dendrite.status_code != 200 or response.dendrite.status_code != 403:
                 self.punish(axon, service="Text-To-Speech", punish_message=response.dendrite.status_message)
             elif response is not None and isinstance(response, lib.protocol.TextToSpeech) and response.speech_output is not None and response.dendrite.status_code == 200:
                 self.handle_speech_output(axon, speech_output, prompt, response.model_name)
