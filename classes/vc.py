93f913154e23626c3131d3e9b33dd80ef8026f47
isharab <155580193+sherijigarx@users.noreply.github.com>
2024-01-18 23:05:05 +0500

    Update vc.py

--- classes/vc.py
+++ classes/vc.py
@@ -223,7 +223,7 @@ class VoiceCloningService(AIModelService):
 
     def process_voice_clone_responses(self, ax):
         try:
-            if self.response.dendrite.status_code != 403:
+            if self.response.dendrite.status_code != 403 or self.response.dendrite.status_code != 200:
                 bt.logging.info(f"Response from {ax.hotkey} for Voice Cloning: {self.response.dendrite.status_code}")
                 self.punish(ax, service="Voice Cloning", punish_message=self.response.dendrite.status_message)
             elif self.response is not None and isinstance(self.response, lib.protocol.VoiceClone) and self.response.clone_output is not None and self.response.dendrite.status_code == 200:
